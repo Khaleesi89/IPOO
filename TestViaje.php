b1c961ae8a4f76dc093180a2dc4b21ba6e5bba26
Khaleesi89 <93682145+Khaleesi89@users.noreply.github.com>
2022-04-03 11:52:31 -0300

    tratando dehacer un menu dentro de un menu

--- TestViaje.php
+++ TestViaje.php
@@ -3,67 +3,104 @@
 include 'Vieje.php';
 
 echo "--------- Viaje Feliz ----------";
-echo "Ingrese los datos correspondientes al viaje \n";
-echo "Ingrese el codigo del viaje: \n";
-$viajeCodigo= strtoupper(trim (fgets(STDIN)));
-echo "Ingrese el destino: \n";
-$lugarDestino= strtoupper(trim (fgets(STDIN)));
-echo "Ingrese la cantidad máxima de asientos: \n";
-$maxAsientos = strtoupper(trim (fgets(STDIN)));
-$objViaje = new Viaje($viajeCodigo,$lugarDestino,$maxAsientos);
 
+// EJECUTAMOS EL PROGRAMA //
 
-echo menu();
-switch ($eleccion) {
-    case '1':
-        // Desea cambiar el código del viaje? //
-        echo "Ingrese el nuevo código: \n";
-        $newCod= strtoupper(trim (fgets(STDIN)));
-        $objViaje->setCodigoViaje($newCod);
-        break;
+// MENU PRINCIPAL //
+
+do{
+    $finish= true;
+    $viagiando= menu();
+    switch ($viagiando) {
         
-    case '2':
-         // Desea cambiar el destino?  //
-        echo "Ingrese el nuevo destino: \n";
-        $newDestino= strtoupper(trim (fgets(STDIN)));
-        $objViaje->setDestino($newDestino);
-        break;
+                            case '1':
+                                    // 1) Cargar un viaje 
+                                    echo "Ingrese los datos correspondientes al viaje \n";
+                                    echo "Ingrese el codigo del viaje: \n";
+                                    $viajeCodigo= strtoupper(trim (fgets(STDIN)));
+                                    echo "Ingrese el destino: \n";
+                                    $lugarDestino= strtoupper(trim (fgets(STDIN)));
+                                    echo "Ingrese la cantidad máxima de asientos: \n";
+                                    $maxAsientos = trim (fgets(STDIN));
+                                    $objViaje = new Viaje($viajeCodigo,$lugarDestino,$maxAsientos);
+                                    echo "Ingrese los datos de los pasajeros: \n";
+                                    $persona=[];
+                                    $persona=infoPasajero();
 
-    case '3':
-         // Desea cambiar la cantidad de pasajeros que viajaron?   //
-        # code...
-        break;
+                            break;
 
-    case '4':
-        // Desea ingresar los datos de un pasajero?  //
-        echo "Ingrese los datos de los pasajeros: \n";
-        if ()
-        $passenger= infoPasajero();
-            break;
+   
 
-    case '5':
-         // Desea cambiar los datos de un pasajero?  //
-             # code...
-             break;
+                            case '2':
+                                // 2) Modificar un viaje
+                                    do{
+                                        $salida=true;
+                                        echo menuPunto2();
+                                        $eleccion2= trim (fgets(STDIN));
 
-    case '6':
-         // Desea cambiar la capacidad máxima de la movilidad?   //
-             # code...
-             break;
+                                        switch ($eleccion2){
+                                            case '1':
+                                                // Desea cambiar el código del viaje? //
+                                                echo "Ingrese el nuevo código: \n";
+                                                $newCod= strtoupper(trim (fgets(STDIN)));
+                                                $objViaje->setCodigoViaje($newCod);
+                                                break;
+                                                
+                                            case '2':
+                                                // Desea cambiar el destino?  //
+                                                echo "Ingrese el nuevo destino: \n";
+                                                $newDestino= strtoupper(trim (fgets(STDIN)));
+                                                $objViaje->setDestino($newDestino);
+                                                break;
+                                            
+                                            case '3':
+                                                // Desea cambiar los datos de un pasajero?  //
+                                                    # code...
+                                                    break;
+                                        
+                                            case '4':
+                                                // Desea cambiar la capacidad máxima de la movilidad?   //
+                                                    
+                                                echo "Ingrese el nuevo valor para la capacidad máxima: \n";
+                                                $capacidadNew=trim (fgets(STDIN));
+                                                $objViaje->setsetCantMaxPasajeros($capacidadNew);
+                                                break;
+                                        
+                                            case '5':
+                                                    //Ver viaje //
+                                                    echo $objViaje;
+                                                    break;
+                                        
+                                            case '6':
+                                                    // Salir  //
+                                                    $salida=false;
+                                                    break;
+                                        
+                                            default:
+                                                echo "el código ingresado no existe";
+                                                break;
+                                       };
+                        
+                            
+                                    }while($salida);
 
-    case '7':
-            //Ver viaje //
-            # code...
-            break;
+                            case '3':
+                                    // 3) Ver datos de un viaje
 
-    case '8':
-            // Salir  //
-            # code...
-            break;
+                                    break;
 
-    default:
-        echo "el código ingresado no existe";
-        break;
+                            case '4':
+                                $finish= false;
+                                break;
+                            default:
+                                echo "Ingrese un valor del 1 al 4";
+
+                            break;
+                    };
+
+
+}while($finish);
+
 
 
 
@@ -81,32 +118,39 @@ switch ($eleccion) {
             echo "Desea ingresar un nuevo pasajero? S - N \n";
             $opcionElegible= strtoupper(trim (fgets(STDIN)));
             if($opcionElegible=="S"){
-
+                $respuesta= true;
             }
-            return $pasajero;
 
-        }
-     
+        }while ($respuesta);
+        return $pasajero;
     }
 
 
 
-    function menu(){
+    function menuPunto2(){
         return $menu = "Elija la opción deseada:
         1- Desea cambiar el código del viaje? \n.
         2- Desea cambiar el destino? \n.
         3- Desea cambiar la cantidad de pasajeros que viajaron?/n.
-        4-Desea ingresar los datos de un pasajero?
-        5- Desea cambiar los datos de un pasajero?\n.
-        6- Desea cambiar la capacidad máxima de la movilidad?\n.
-        7- Ver viaje \n.
-        8- Salir \n.";
+        4- Desea cambiar los datos de un pasajero?\n.
+        5- Desea cambiar la capacidad máxima de la movilidad?\n.
+        6- Ver viaje \n.
+        7- Salir \n.";
 
 
 
     }
 
-
-    
-
+    function menu(){
+        do{
+            echo "Ingrese una opción:\n\n
+            1) Cargar un viaje \n
+            2) Modificar un viaje \n
+            3) Ver datos de un viaje \n
+            4) Salir\n";
+            $respuesta =trim(fgets(STDIN));
+            if (!(is_int($respuesta)) && ($respuesta < 0 || $respuesta > 4)) {
+                echo "Opcion incorrecta.\n";
+            }    
+    }while (((is_int($respuesta)) && ($respuesta < 0 || $respuesta < 4)));
 }
